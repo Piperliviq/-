a2a5af59eff434796efebc00e872788e0e7db4ef
Piperliviq <116745989+Piperliviq@users.noreply.github.com>
2022-11-23 22:09:26 +0200

    Update zad3.py
    стана грешка и не се качи

--- LAB5/zad3.py
+++ LAB5/zad3.py
@@ -1,3 +1,25 @@
-first_num = input("First number is ")
-second_num = input("Second number is ")
-print(int(first_num) - int(second_num))
+def sum_nums(a, b):
+    return a+b
+
+def sub_nums(a, b):
+    return a-b
+
+def dev_nums(a, b):
+    return a/b
+
+def multi_nums(a, b):
+    return a*b
+
+print("Enter an operator: + - / * ")
+
+op_type = input()
+a,b = eval(input("Enter two numbers: "))
+
+if op_type == "+":
+    print(sum_nums(a,b))
+elif op_type == "-":
+    print(sub_nums(a,b))
+elif op_type == "/":
+    print(dev_nums(a,b))
+elif op_type == "*":
+    print(multi_nums(a,b))
